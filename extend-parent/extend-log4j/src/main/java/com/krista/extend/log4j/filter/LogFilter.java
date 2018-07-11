2cb076592dd6240cf6aaacbf01eb60cee6bd3b2d
wangdh <1366678737@qq.com>
2018-07-11 14:27:45 +0800

    log4j扩展

---
+++ extend-parent/extend-log4j/src/main/java/com/krista/extend/log4j/filter/LogFilter.java
@@ -0,0 +1,47 @@
+package com.krista.extend.log4j.filter;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.MDC;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.util.ServletContextPropertyUtils;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @Auther: dw_wanghonghong
+ */
+public class LogFilter implements Filter {
+    private static String logKey = "";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String location = filterConfig.getInitParameter("log4jConfigLocation");
+        if (location != null) {
+            ServletContext servletContext = filterConfig.getServletContext();
+            servletContext.log("Initializing log4j from [" + this.getClass().getResource("/" + location).getPath() + "]");
+            System.setProperty("webapp.root", servletContext.getRealPath("/"));
+            PropertyConfigurator.configure(this.getClass().getResourceAsStream("/" + location));
+        }
+
+        logKey = filterConfig.getInitParameter("logKey");
+        if(logKey == null || logKey==""){
+            logKey = "requestId";
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        MDC.put(logKey, UUID.randomUUID().toString().replace("-","").substring(0,10));
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
