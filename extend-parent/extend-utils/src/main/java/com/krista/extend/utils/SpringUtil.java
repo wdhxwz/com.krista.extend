bf8822c40ee3555f77a4183ccf9a8144e6733bbc
wangdh <1366678737@qq.com>
2018-07-11 20:45:06 +0800

    封装springcontext

---
+++ extend-parent/extend-utils/src/main/java/com/krista/extend/utils/SpringUtil.java
@@ -0,0 +1,25 @@
+package com.krista.extend.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: dw_wanghonghong
+ * @Date: 2018/7/11 20:38
+ * @Description:
+ */
+@Component
+public class SpringUtil implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static<T> T getBean(Class<T> clz){
+        return applicationContext.getBean(clz);
+    }
+}
