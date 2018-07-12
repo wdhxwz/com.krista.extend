68a6e5c6b2d39cdda6fbeccff9b89fa52c71e38b
wangdh <1366678737@qq.com>
2018-07-12 10:02:05 +0800

    记录的日志字段可配置，可从请求参数中获取

--- extend-parent/extend-log4j/src/main/java/com/krista/extend/log4j/filter/LogFilter.java
+++ extend-parent/extend-log4j/src/main/java/com/krista/extend/log4j/filter/LogFilter.java
@@ -26,41 +26,49 @@ public class LogFilter implements Filter {
         if (location != null) {
             ServletContext servletContext = filterConfig.getServletContext();
             servletContext.log("Initializing log4j from [" + this.getClass().getResource("/" + location).getPath() + "]");
-            String logRootPath = null;
 
-            try{
-                logRootPath = AppConfigUtil.getProperty("log.root.path");
-            }catch (Exception ex){
-                ex.printStackTrace();
-            }
-
-            if(StringUtil.isEmpty(logRootPath)){
-                logRootPath = servletContext.getInitParameter("log.root.path");
-            }
-
-            if(StringUtil.isEmpty(logRootPath)){
-                logRootPath = servletContext.getRealPath("/");
-            }
+            String logRootPath = getConfigValue("log.root.path",servletContext,servletContext.getRealPath("/"));
             servletContext.log("log.root.path = " + logRootPath);
-            System.setProperty("webapp.root", logRootPath);
 
+            System.setProperty("webapp.root", logRootPath);
             PropertyConfigurator.configure(this.getClass().getResourceAsStream("/" + location));
         }
 
-        logKey = filterConfig.getInitParameter("logKey");
-        if(logKey == null || logKey==""){
-            logKey = "requestId";
-        }
+        logKey = getConfigValue("log.key",filterConfig.getServletContext(),"requestId");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        MDC.put(logKey, UUID.randomUUID().toString().replace("-","").substring(0,10));
+        String requestId  = servletRequest.getParameter(logKey);
+        if(StringUtil.isEmpty(requestId)){
+            requestId = UUID.randomUUID().toString().replace("-","").substring(0,10);
+        }
+
+        MDC.put("requestId", requestId);
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
     public void destroy() {
 
+    }
+
+    private String getConfigValue(String key,ServletContext servletContext,String defaultValue){
+        String returnValue = "";
+        try{
+            returnValue = AppConfigUtil.getProperty(key);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        if(StringUtil.isEmpty(returnValue)){
+            returnValue = servletContext.getInitParameter(key);
+        }
+
+        if(StringUtil.isEmpty(returnValue)){
+            returnValue = defaultValue;
+        }
+
+        return returnValue;
     }
 }
