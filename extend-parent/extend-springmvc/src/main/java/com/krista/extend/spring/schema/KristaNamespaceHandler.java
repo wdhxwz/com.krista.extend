29b73e9768529bfa798d48d25de3e721b3bf1ac9
wangdh <1366678737@qq.com>
2018-07-31 17:57:27 +0800

    提交schemas文件

---
+++ extend-parent/extend-springmvc/src/main/java/com/krista/extend/spring/schema/KristaNamespaceHandler.java
@@ -0,0 +1,15 @@
+package com.krista.extend.spring.schema;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * @Auther: dw_wanghonghong
+ * @Date: 2018/7/31 17:52
+ * @Description:
+ */
+public class KristaNamespaceHandler extends NamespaceHandlerSupport {
+    @Override
+    public void init() {
+
+    }
+}
