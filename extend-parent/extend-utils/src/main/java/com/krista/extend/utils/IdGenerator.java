c27909177905d0c0d925c4eba9900057079fa135
wangdh <1366678737@qq.com>
2019-02-01 11:03:02 +0800

    33333

--- extend-parent/extend-utils/src/main/java/com/krista/extend/utils/IdGenerator.java
+++ extend-parent/extend-utils/src/main/java/com/krista/extend/utils/IdGenerator.java
@@ -92,9 +92,8 @@ public class IdGenerator {
         return ts;
     }
 
-    public static final int getProcessID() {
+    public static int getProcessID() {
         RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
-        return Integer.valueOf(runtimeMXBean.getName().split("@")[0])
-                .intValue();
+        return Integer.valueOf(runtimeMXBean.getName().split("@")[0]);
     }
 }
